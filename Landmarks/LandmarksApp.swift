a6a88ea8a4e22c9f9b6168a353dfe91251a829ca
Daniel Copley <djcopley@users.noreply.github.com>
2022-01-08 22:16:50 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Daniel Copley on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
