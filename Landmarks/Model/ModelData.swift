42ff6721a789b9fa8ff012597630fa5d7b8a4c51
Daniel Copley <djcopley@users.noreply.github.com>
2022-01-09 18:31:57 -0700

    Finished part 4

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -31,4 +31,5 @@ func load<T: Decodable>(_ filename: String) -> T {
 
 class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
 }
